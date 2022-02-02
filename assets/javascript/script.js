65082ce6a59259bb5d31d32495641b85749d1ecc
Shahen Tokhalyan <tokhalyan@gmail.com>
2022-02-01 17:23:12 -0800

    code quality update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,18 @@
+// Global variables with query serlectors
 const cityInputEl = document.querySelector("#city-search-input");
 const cityFormEl = document.querySelector("#city-form");
 const cityContainerEl = document.querySelector("#city-container");
 const currentDateEl = document.querySelector("#current-date");
 const fiveDayEl = document.querySelector("fiveDayForecastContainer");
 const forecastEls = document.querySelectorAll(".forecast");
-let displayEl = document.getElementById("display");
+const displayEl = document.getElementById("display");
+// api key for using 
 const apiKey = "5a60081ec2c71a1391cbafc0e63f0ec9";
-
+// getting current date from moment JS, format - MM/DD/YYYY
 const currentDate = moment().format('L');
 currentDateEl.textContent = `(${currentDate})`;
 
+// Getting city name from user input
 function getCityName (event) {
     // prevent page from refreshing
     event.preventDefault();
@@ -28,6 +31,7 @@ function getCityName (event) {
     }
 };
 
+// Receiving city name from getCityName function and getting weather information for the city
 function getCityInfo(cityInfo) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInfo}&appid=${apiKey}&units=imperial`;
     fetch(apiUrl)
@@ -62,11 +66,11 @@ function getCityInfo(cityInfo) {
     )
 
     .catch(function(error) {
-        // console.log(error);
-        // alert("Unable to connect to openweathermap.org");
+        alert("Unable to connect to openweathermap.org");
     })
 }
 
+// using local storage to save user's inputs 
 function saveSearchHistory() {
     if(cityInputEl.value) {
 
@@ -86,6 +90,7 @@ function saveSearchHistory() {
     }
 }
 
+// getting information from local storage to show search logs on the page
 function showSearchHistory() {
     if(localStorage.getItem("city")) {
         let borderTop = document.createElement("div");
@@ -95,6 +100,7 @@ function showSearchHistory() {
         cityContainerEl.innerHTML = "";
         
         let storageCityName = JSON.parse(localStorage.getItem("city"));
+        // using revers method to print users last input info on top of the list
         storageCityName.reverse();
         
 
@@ -110,6 +116,7 @@ function showSearchHistory() {
                 return
             })
             
+            // search logs only show last 8 inputs, when storage length is bigger than 8 it remove's the last value from the storage
             if(storageCityName.length > 8) {
                 storageCityName.pop();
             }
@@ -118,12 +125,9 @@ function showSearchHistory() {
     }
 }
 
-function k2f(K) {
-        return Math.floor((K - 273.15) * 1.8 + 32);
-    }
-
+// receiving city name from getCityName function and showing 5 day forecast 
 function fiveDayForecast(fiveDayCity) {
-    let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + fiveDayCity + "&appid=" + apiKey;
+    let forecastQueryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${fiveDayCity}&appid=${apiKey}&units=imperial`;
         fetch(forecastQueryURL)
             .then(function (response) {
                 return response.json()
@@ -144,19 +148,21 @@ function fiveDayForecast(fiveDayCity) {
 
                     // Icon for current weather
                     const forecastWeatherEl = document.createElement("img");
-                    forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[forecastIndex].weather[0].icon + "@2x.png");
-                    forecastWeatherEl.setAttribute("alt", data.list[forecastIndex].weather[0].description);
+                    forecastWeatherEl.setAttribute("src", `https://openweathermap.org/img/wn/${data.list[forecastIndex].weather[0].icon}@2x.png`);
                     forecastEls[i].append(forecastWeatherEl);
+
                     const forecastTempEl = document.createElement("p");
-                    forecastTempEl.innerHTML = "Temp: " + k2f(data.list[forecastIndex].main.temp) + " &#176F";
+                    forecastTempEl.innerHTML = `Temp: ${data.list[forecastIndex].main.temp} °F`;
                     forecastEls[i].append(forecastTempEl);
+                    
                     const forecastHumidityEl = document.createElement("p");
-                    forecastHumidityEl.innerHTML = "Humidity: " + data.list[forecastIndex].main.humidity + "%";
+                    forecastHumidityEl.innerHTML = `Humidity: ${data.list[forecastIndex].main.humidity} %`;
                     forecastEls[i].append(forecastHumidityEl);
                 }
             })
 }
+
+// calling getCityName function when user pushes submit button 
 cityFormEl.addEventListener("submit", getCityName);
 
 showSearchHistory()
-// fiveDayCity
